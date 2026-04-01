0d3fdd19f4340e8709ed3ae36305b7ca1c328b44
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2026-04-01 17:00:15 -0300

    Adicionando a lista de guildas e realizando ajustes na classe entidade

--- src/main/java/com/LigaAcademic/AcademicProject/model/Membro.java
+++ src/main/java/com/LigaAcademic/AcademicProject/model/Membro.java
@@ -1,19 +1,38 @@
 package com.LigaAcademic.AcademicProject.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "membros")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Membro {
 
     private String nome;
+
     private String cargo;
+
     @Column(unique = true,length = 11)
     private String matricula;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String email;
+
+    @ManyToMany
+    @JoinTable(name = "relacao_guildas",
+    joinColumns = @JoinColumn(name = "membro_id"),
+    inverseJoinColumns = @JoinColumn(name = "guilda_model_id")
+    )
+    private List<GuildasModel> guildasModel;
+
+
 
 
     public String getNome() {
