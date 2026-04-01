0973ab9d25e617797fb72c1528e80001fea684eb
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2026-04-01 17:02:05 -0300

    Criando a classe repositório

---
+++ src/main/java/com/LigaAcademic/AcademicProject/repository/GuildasRepository.java
@@ -0,0 +1,9 @@
+package com.LigaAcademic.AcademicProject.repository;
+
+import com.LigaAcademic.AcademicProject.model.GuildasModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuildasRepository  extends JpaRepository<GuildasModel, Long > {
+
+
+}
