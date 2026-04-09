63d56bb1c3c33ad2f971467448c633bed7a1a4a8
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2026-04-09 09:34:23 -0300

    bugfix: Corrigindo erro na entidade manipulada

--- src/main/java/com/LigaAcademic/AcademicProject/repository/GuildasRepository.java
+++ src/main/java/com/LigaAcademic/AcademicProject/repository/GuildasRepository.java
@@ -3,7 +3,7 @@ package com.LigaAcademic.AcademicProject.repository;
 import com.LigaAcademic.AcademicProject.model.GuildasModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GuildasRepository  extends JpaRepository<GuildasModel, Long > {
+public interface GuildasRepository  extends JpaRepository<GuildasModel, Long> {
 
 
 }
