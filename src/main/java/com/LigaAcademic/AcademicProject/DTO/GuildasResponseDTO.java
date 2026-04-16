b98e3b9d501e40cc0f2799e4443379a99745b1cb
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2026-04-16 16:45:56 -0300

    refactor: Realocando os DTO para um packege separado.

---
+++ src/main/java/com/LigaAcademic/AcademicProject/DTO/GuildasResponseDTO.java
@@ -0,0 +1,10 @@
+package com.LigaAcademic.AcademicProject.DTO;
+
+public record GuildasResponseDTO(
+
+        Long id,
+        String tutor_guildas,
+        String nome_guilda,
+        int quantidade_pessoas
+) {
+}
--- src/main/java/com/LigaAcademic/AcademicProject/User/RegisterDTO.java
+++
@@ -1,8 +0,0 @@
-package com.LigaAcademic.AcademicProject.User;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-
-public record RegisterDTO(String email, String password, UsersRoles role) {
-
-}
