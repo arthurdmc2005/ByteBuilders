67d708bca565504e8505257f35e41d186dfe41e0
arthurdmc2005 <arthurdiasmoreiracorrea@gmail.com>
2026-04-17 18:20:34 -0300

    Adicionando a notação @Query para forçar a utilização de um LEFT JOIN FETCH e não um INNER, para casos em que o membro não tiver vinculado a nenhuma guilda

--- src/main/java/com/LigaAcademic/AcademicProject/repository/MembroRepository.java
+++ src/main/java/com/LigaAcademic/AcademicProject/repository/MembroRepository.java
@@ -2,14 +2,22 @@ package com.LigaAcademic.AcademicProject.repository;
 
 import com.LigaAcademic.AcademicProject.model.Membro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MembroRepository extends JpaRepository<Membro,Long> {
 
     Optional <Membro> findByMatricula(String matricula);
+
+    @Query("SELECT m FROM Membro m LEFT JOIN FETCH m.guildasModel ")
+    List<Membro> buscarTodosComGuildas();
+
+    @Query("SELECT m FROM Membro m LEFT JOIN FETCH m.guildasModel WHERE m.matricula = :matricula")
+    Optional<Membro> findByMatriculaComGuildas(String matricula);
 
 }
 
